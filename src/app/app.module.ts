0e0cbcbe54e4be1d12535b7d97ff1cb63a302233
Otto Sterner <ottosterner1@gmail.com>
2023-05-01 16:41:14 +0100

    header and main-body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainBodyComponent } from './main-body/main-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainBodyComponent
   ],
   imports: [
     BrowserModule,
